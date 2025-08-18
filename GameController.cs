af6b8a28df1fdb2892786ae0383f453cd6a0eaf8
Filippo027 <filippomartellossi@gmail.com>
2025-08-18 17:25:48 +0200

    Update GameController.cs

--- GameController.cs
+++ GameController.cs
@@ -1,44 +1,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour{
+public class GameController : MonoBehaviour {
     public static GameController Instance { get; private set; }
-    
+
     public RotteManager RotteManager { get; private set; }
 
-    void Awake(){
-        if (Instance == null){
+    void Awake() {
+        if (Instance == null) {
             Instance = this;
             DontDestroyOnLoad(gameObject);
             InizializzaGioco();
-        }else{
+        } else {
             Destroy(gameObject);
         }
     }
 
-    private void InizializzaGioco(){
+    private void InizializzaGioco() {
         RotteManager = new RotteManager();
 
-        // Pianeti di esempio
-        Pianeta terra = new Pianeta("Terra", new Vector3(0, 0, 0), 8000000000, 1000f);
-        Pianeta marte = new Pianeta("Marte", new Vector3(100, 0, 0), 2000000, 500f);
-        Pianeta giove = new Pianeta("Giove", new Vector3(300, 0, 0), 0, 2000f);
+        // Pianeti di esempio 
+        Pianeta terra = new Pianeta("p1", "Terra", new Vector2(0, 0));
+        Pianeta marte = new Pianeta("p2", "Marte", new Vector2(5, 0));
+        Pianeta giove = new Pianeta("p3", "Giove", new Vector2(10, 0));
 
-        // Rotte di esempio
-        RotteManager.AggiungiRotta(new RottaStandard("Terra-Marte", terra, marte, 100, 500));
-        RotteManager.AggiungiRotta(new RottaStandard("Terra-Giove", terra, giove, 200, 800));
-        RotteManager.AggiungiRotta(new RottaStandard("Marte-Giove", marte, giove, 150, 600));
+        // Rotte di esempio 
+        RotteManager.AggiungiRotta(new RottaStandard(terra, marte));
+        RotteManager.AggiungiRotta(new RottaStandard(terra, giove));
+        RotteManager.AggiungiRotta(new RottaStandard(marte, giove));
     }
 
-    public List<IRottaCommerciale> CercaRotte(string keyword){
+    // Accesso pubblico per la UI
+    public List<IRottaCommerciale> CercaRotte(string keyword) {
         return RotteManager.CercaPerNome(keyword);
     }
 
-    public List<IRottaCommerciale> GetRotte(){
+    public List<IRottaCommerciale> GetRotte() {
         return RotteManager.GetRotte();
     }
 
-    public void OrdinaRottePerProfitto(){
+    public void OrdinaRottePerProfitto() {
         RotteManager.OrdinaPerProfittoDecrescente();
     }
 }
