bd16bc242799fe17d63f6dc3cdceeca9376df7ec
Filippo027 <filippomartellossi@gmail.com>
2025-08-18 17:58:01 +0200

    Update Nave.cs

--- Nave.cs
+++ Nave.cs
@@ -1,23 +1,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Nave : EntitaSpaziale{
+public abstract class Nave : EntitaSpaziale {
     public int Capacita { get; protected set; }
     public float Carburante { get; protected set; }
-    public Dictionary<Risorsa,int> Inventario { get; private set; }
+    public Dictionary<Risorsa, int> Inventario { get; private set; }
     public Queue<ComandoNave> QueueComandi { get; private set; }
 
-    public Nave(string id, Vector2 pos, int capacita, float carburante) : base(id, pos){
+    public Nave(string id, Vector2 pos, int capacita, float carburante) : base(id, pos) {
         Capacita = capacita;
         Carburante = carburante;
-        Inventario = new Dictionary<Risorsa,int>();
+        Inventario = new Dictionary<Risorsa, int>();
         QueueComandi = new Queue<ComandoNave>();
     }
 
     public abstract void EseguiProssimoComando();
     public abstract float CalcolaConsumo(float distanza);
 
-    public void AggiungiComando(ComandoNave c){
+    public void AggiungiComando(ComandoNave c) {
         QueueComandi.Enqueue(c);
     }
 }
